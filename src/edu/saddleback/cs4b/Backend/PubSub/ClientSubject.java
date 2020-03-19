d77d91882494f957d718985c8530dbabb30559c0
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-18 20:47:57 -0700

    ClientObserver/Server abstract methods in place

--- src/edu/saddleback/cs4b/Backend/PubSub/ClientSubject.java
+++ src/edu/saddleback/cs4b/Backend/PubSub/ClientSubject.java
@@ -1,4 +1,7 @@
 package edu.saddleback.cs4b.Backend.PubSub;
 
 public interface ClientSubject {
+    void registerObserver(ClientObserver o);
+    void removeObserver(ClientObserver o);
+    void notifyObservers();
 }
