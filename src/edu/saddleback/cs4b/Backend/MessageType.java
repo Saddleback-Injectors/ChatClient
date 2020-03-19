d28e1a444170c98b04d41469c5ce4ff9c3760ff4
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-18 20:51:49 -0700

    MessageType enum init and written

---
+++ src/edu/saddleback/cs4b/Backend/MessageType.java
@@ -0,0 +1,16 @@
+package edu.saddleback.cs4b.Backend;
+
+public enum MessageType {
+    TEXT("Text"),
+    REGISTRATION("Registration"),
+    IMAGE("Image"),
+    UPDATE("Update"),
+    DISCONNECT("Disconnection");
+
+    private String type;
+    private MessageType(String type) {
+        this.type = type;
+    }
+    
+    public String getType() { return type; }
+}
