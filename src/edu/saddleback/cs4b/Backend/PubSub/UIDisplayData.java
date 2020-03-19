c3235a4ca61b4bbf1d24dc1c08e793fe3b9a2af3
Tucker Harvey <coopert.harvey@gmail.com>
2020-03-18 22:18:50 -0700

    refactor ui display data

--- src/edu/saddleback/cs4b/Backend/PubSub/UIDisplayData.java
+++ src/edu/saddleback/cs4b/Backend/PubSub/UIDisplayData.java
@@ -2,12 +2,22 @@ package edu.saddleback.cs4b.Backend.PubSub;
 
 public class UIDisplayData implements Receivable {
     private Receivable type;
-    public UIDisplayData(Receivable type) {
+    private Object data;
+    private String destination;
+
+    public UIDisplayData(Receivable type, Object data, String dst) {
         this.type = type;
+        this.data = data;
+        this.destination = dst;
+    }
+
+    public UIDisplayData(Receivable type, Object data) {
+        this(type, data, "");
     }
 
     @Override
     public String getType() {
         return type.getType();
     }
+    public Object getData() { return data; }
 }
