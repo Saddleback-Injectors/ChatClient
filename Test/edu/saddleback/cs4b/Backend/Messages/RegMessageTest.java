b884ff280a699437f323f1d5e9925a8724e60d65
Isaac <socalisaac@yahoo.com>
2020-03-21 18:37:43 -0700

    Test Type for Reg Message

---
+++ Test/edu/saddleback/cs4b/Backend/Messages/RegMessageTest.java
@@ -0,0 +1,140 @@
+package edu.saddleback.cs4b.Backend.Messages;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegMessageTest
+{
+    @Test
+    @DisplayName("Test Type for Reg-Msg")
+    void testTypeForDisMsg()
+    {
+        byte[] test = new byte[6];
+        List<String> channels = new List<>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<String> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends String> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, Collection<? extends String> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public String get(int index) {
+                return null;
+            }
+
+            @Override
+            public String set(int index, String element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, String element) {
+
+            }
+
+            @Override
+            public String remove(int index) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public ListIterator<String> listIterator() {
+                return null;
+            }
+
+            @Override
+            public ListIterator<String> listIterator(int index) {
+                return null;
+            }
+
+            @Override
+            public List<String> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+        };
+
+        RegMessage ms = new RegMessage("Test", "User", channels);
+        assertEquals("Reg-Msg", ms.getType());
+    }
+}
